909afaa785946ba4d43f26d70c466017baa543ea
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-06-12 13:10:16 -0700

    feat: add authentication via JWT (#35)

--- src/webtrigger/utils/responseBuilder.ts
+++ src/webtrigger/utils/responseBuilder.ts
@@ -1,8 +1,6 @@
-import {
-  EmptyRequestBodyError,
-  InvalidPayloadError,
-  MissingRequestBodyError,
-} from '../types/errors';
+import { TokenExpiredError } from 'jsonwebtoken';
+
+import * as Errors from '../types/errors';
 import { WebTriggerResponse } from '../types/types';
 
 export function buildResponse(body: object | string, statusCode: number): WebTriggerResponse {
@@ -14,11 +12,14 @@ export function buildResponse(body: object | string, statusCode: number): WebTri
 }
 
 export function buildErrorResponse(error: unknown): WebTriggerResponse {
-  if (error instanceof MissingRequestBodyError || error instanceof EmptyRequestBodyError) {
+  if (
+    error instanceof Errors.MissingRequestBodyError ||
+    error instanceof Errors.EmptyRequestBodyError
+  ) {
     return buildResponse({ error: error.message }, 400);
   }
 
-  if (error instanceof InvalidPayloadError) {
+  if (error instanceof Errors.InvalidPayloadError) {
     return buildResponse(
       {
         error: error.message,
@@ -29,11 +30,36 @@ export function buildErrorResponse(error: unknown): WebTriggerResponse {
     );
   }
 
+  if (error instanceof TokenExpiredError) {
+    return buildResponse(
+      { error: error.message, message: 'Please try again with a new token.' },
+      401,
+    );
+  }
+
+  if (
+    error instanceof Errors.EmptyAuthHeaderError ||
+    error instanceof Errors.FailedToFindJwkError ||
+    error instanceof Errors.InvalidTokenError ||
+    error instanceof Errors.MissingAuthHeaderError ||
+    error instanceof Errors.MissingKeyIdError
+  ) {
+    return buildResponse({ error: error.message }, 401);
+  }
+
+  if (
+    error instanceof Errors.MissingOrganizationIdError ||
+    error instanceof Errors.FailedToFetchJwksError
+  ) {
+    return buildResponse({ error: error.message }, 500);
+  }
+
   return buildResponse(
     {
-      error: `An unexpected error occurred. Please try again or open an issue. ${
-        (<Error>error).message
-      }`,
+      error: 'An unexpected error occurred. Please try again or open an issue.',
+      message: (<Error>error).message,
+      stack: (<Error>error).stack,
+      name: (<Error>error).name,
     },
     500,
   );
