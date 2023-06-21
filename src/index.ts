3f5d6bfc7361cc83264ea3006fca069572c6435c
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-06-21 11:38:10 -0700

    feat: export new pages for manifest

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { runAdminPage } from './admin-page/AdminPage';
+import { runConfigurePage } from './admin-page/Configure';
+import { runGetStartedPage } from './admin-page/getStarted';
 import { handleOrbRequest } from './webtrigger/handle-orb-request';
 
 // Web Triggers
-export { handleOrbRequest };
+export { handleOrbRequest, runAdminPage, runConfigurePage, runGetStartedPage };
