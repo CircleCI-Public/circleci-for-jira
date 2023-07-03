9ed1e463f0e8db17a016b1cc4cf27f514fe14b64
KyleTryon <Kyle.Tryon@circleci.com>
2023-07-02 21:12:28 -0400

    feat: initialize admin-home template

---
+++ apps/admin-home/src/theme.tsx
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles';
+
+const AtlassianTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052CC',
+      dark: '#172B4D',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#6554C0',
+    },
+    grey: {
+      100: '#FFFFFF',
+      200: '#FAFBFC',
+      300: '#F4F5F7',
+      400: '#EBECF0',
+      500: '#DFE1E6',
+      600: '#C1C7D0',
+    },
+  },
+});
+
+export default AtlassianTheme;
