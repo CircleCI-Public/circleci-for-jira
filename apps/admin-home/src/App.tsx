ee465929bbc6e7f0c527f57db40d1c6eeb7f5acc
KyleTryon <Kyle.Tryon@circleci.com>
2023-07-02 21:12:28 -0400

    feat: initialize admin-home template

--- apps/admin-home/src/App.tsx
+++ apps/admin-home/src/App.tsx
@@ -1,40 +1,72 @@
 import './App.css';
 
+import { ThemeProvider } from '@emotion/react';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import { Link } from 'ui';
 
-import viteLogo from '/vite.svg';
+import AtlassianTheme from './theme';
 
-import reactLogo from './assets/react.svg';
+const InfoBox = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.grey[400],
+  padding: theme.spacing(2),
+  textAlign: 'left',
+  borderRadius: theme.spacing(1),
+}));
+
+const ButtonBox = styled('div')(({ theme }) => ({
+  margin: theme.spacing(2),
+  '& > button': {
+    margin: theme.spacing(1),
+  },
+}));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div>
-        <Link href={'https://google.com'}>I&apos;m a shared component</Link>
-      </div>
-      <div>
-        <Button variant='contained'>I&apos;m a MUI button</Button>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <ThemeProvider theme={AtlassianTheme}>
+        <div className='App'>
+          <h1>CircleCI For Jira</h1>
+          <section>
+            <InfoBox>
+              <p>
+                Version:&nbsp;
+                <code>1.0.0</code>
+              </p>
+              <p>
+                Marketplace:&nbsp;
+                <Link href='https://marketplace.atlassian.com/apps/1215946/circleci-for-jira'>
+                  CircleCI For Jira
+                </Link>
+              </p>
+              <p>
+                Documentation:&nbsp;
+                <Link href='https://circleci.com/docs/jira-plugin/'>CircleCI Docs</Link>
+              </p>
+              <p>
+                Support:&nbsp;
+                <Link href='https://support.circleci.com/hc/en-us/requests/new?ticket_form_id=855268'>
+                  Submit a ticket
+                </Link>
+              </p>
+              <p>
+                Submit bugs:&nbsp;
+                <Link href='https://github.com/CircleCI-Public/circleci-for-jira'>Github</Link>
+              </p>
+            </InfoBox>
+          </section>
+          <section>
+            <ButtonBox>
+              <Button variant='contained' color='primary'>
+                Configure
+              </Button>
+              <Button variant='contained' color='secondary' href=''>
+                Get Started
+              </Button>
+            </ButtonBox>
+          </section>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
