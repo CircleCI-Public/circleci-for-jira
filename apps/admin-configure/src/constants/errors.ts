ab6a6ea52282591d6df850beb56c5cb1f06e8388
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-07-06 18:16:12 -0700

    feat: replace `Alert` with `Toast`

---
+++ apps/admin-configure/src/constants/errors.ts
@@ -0,0 +1,7 @@
+export const WEB_TRIGGER_QUERY_ERROR =
+  'Something went wrong while fetching your web trigger URL. Please refresh the page or open an issue.';
+export const FORM_DATA_QUERY_ERROR =
+  'Something went wrong while fetching your data! Please refresh the page or open an issue.';
+export const FORM_DATA_MUTATION_ERROR =
+  'Something went wrong while saving your changes. Please try again or open an issue.';
+export const FORM_DATA_MUTATION_SUCCESS = 'Your changes have been saved!';
--- apps/admin-configure/src/main.tsx
+++ apps/admin-configure/src/main.tsx
@@ -1,11 +1,26 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { toast, ToastContainer } from 'react-toastify';
 
 import App from './App';
 
 const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (_error, query) => {
+      if (query?.meta?.errorMessage) {
+        toast.error(query.meta.errorMessage as string);
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onSuccess: (_data, _variables, _context, mutation) => {
+      if (mutation?.meta?.successMessage) {
+        toast.success(mutation?.meta?.successMessage as string);
+      }
+    },
+  }),
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
@@ -18,6 +33,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer position='top-right' autoClose={2500} closeOnClick pauseOnHover theme='light' />
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
